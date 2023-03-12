29d88f64b1e403be243877c6db40c08030fa93fb
⊰⊹Ｂｕｎｎｙ⊹⊱ <126992653+Bunnylby@users.noreply.github.com>
2023-03-12 01:37:23 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,26 @@
-/*
-
-Obejtivo- quando clicar no botao trocar a imagem correspondente.
-
-parte 1- pegar html dos botoes
-
-parte 2- identificar o clique do usuario no botao
-
-parte 3- desmarcar o botao anterior
-
-parte 4- marcar o botao clicado como selecionado
-
-parte 5- enconder a imagem anterior
-
-parte 6- aparecer a imagem correspondente ao botao
-
-*/
-
-// passo 1
-
 const botoesCarrossel = document.querySelectorAll('.botao');
+
 
 const imagens = document.querySelectorAll('.imagem');
 
-// passo 2
 
 botoesCarrossel.forEach((botao, indice) => {
 
   botao.addEventListener('click', () => {
 
-    //passo 3
-
     const botaoSelecionado = document.querySelector('.selecionado');
 
     botaoSelecionado.classList.remove('selecionado');
-
-    // passo 4
+    
 
     botao.classList.add('selecionado');
-
-    // passo 5
+    
 
     const imagemAtiva = document.querySelector('.ativa');
 
     imagemAtiva.classList.remove('ativa');
-
-    // passo 6
-
+    
+    
     imagens[indice].classList.add('ativa');
 
   })
